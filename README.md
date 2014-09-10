e0f303958201fd8b7e5339ad630afe8dfa484c6f
yohannr <kozher20@hotmail.com>
2014-09-10 21:35:07 +0200

    Return last comment
    - add private function getLastComment()
    - manage date format

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Simple tool which extract open/new issues from Bitbucket.
 
 ## TODO
 
-- manage date format and french/english (regarding browser)
-- get last comments, https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/issues/{issue_id}/comments
+- manage french/english (regarding browser)
 - graphic interface with table and why not a way to send the report by mail or send a mail for a dedicated issue
 - order by priority (critical, major, minor, trivial), type (bug, enhancement, task, proposal) and add color 
 
--- bitbucket.class.php
+++ bitbucket.class.php
@@ -72,6 +72,58 @@ class Bitbucket
 
 
 	/*
+	 * Return last comment of an issue
+	 * API reference : https://bitbucket.org/api/1.0/repositories/{accountname}/{repo_slug}/issues/{issue_id}/comments
+	*/
+	private function getLastComment($id)
+	{
+		$url = 'https://bitbucket.org/api/1.0/repositories/'.$this->accountname.'/'.$this->repo_slug.'/issues/'.$id.'/comments';
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, $this->user . ":" . $this->pwd);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		if ($info['http_code'] != 200) {
+			echo 'Error HTTP '.$info['http_code'];
+			return false;
+		}
+
+		$arr_issue = array();
+		$arr_issue = json_decode($response);
+
+		if (empty($arr_issue)) {
+			return '';
+		}
+		else {
+			$comment = $arr_issue[0]->content;
+			$author = $arr_issue[0]->author_info->display_name.' - '.$this->convertBitbucketDate($arr_issue[0]->utc_updated_on);
+			return $comment.' ['.$author.']';
+		}
+
+		
+	}
+
+
+	/*
+	 * Convert Bitbucket date (ex : 2014-07-08 23:30:23+00:00) into convenient date without hour
+	*/
+	private function convertBitbucketDate($date)
+	{
+		$arr_date = date_parse($date);
+		$timestamp = mktime($arr_date['hour'], $arr_date['minute'], $arr_date['second'], $arr_date['month'], $arr_date['day'], $arr_date['year']);
+		return strftime("%d %B %Y", $timestamp);
+	}
+
+
+	/*
 	 * Export issues into Excel file, using PHPExcel library
 	*/
 	public function exportExcel($arr_issues)
@@ -84,50 +136,48 @@ class Bitbucket
 		$sheet = $objPHPExcel->getActiveSheet();
 		$sheet->setTitle('Issues');
 
-		// Define columns (Id, Type, Priority, Title, Reported_by, Responsible, Content, Status, Created, Last_updated)
+		// Define columns (Id 6, Type 16, Priority 12, Title 72, Reported_by 22, Responsible 22, Content 65, Status 10, Created 26, Last_updated 26)
 		// NB : columns in bitbuckets --> Id, Title, Type, Priority, Status, Assignee, Created, Updated
-		$sheet->SetCellValue('A1', 'Id');
+		$sheet->SetCellValue('A1', 'Title');
 		$sheet->SetCellValue('B1', 'Type');
 		$sheet->SetCellValue('C1', 'Priority');
-		$sheet->SetCellValue('D1', 'Title');
-		$sheet->SetCellValue('E1', 'Reported_by');
-		$sheet->SetCellValue('F1', 'Responsible');
-		$sheet->SetCellValue('G1', 'Content');
-		$sheet->SetCellValue('H1', 'Status');
+		$sheet->SetCellValue('D1', 'Content');
+		$sheet->SetCellValue('E1', 'Last_comment');
+		$sheet->SetCellValue('F1', 'Status');
+		$sheet->SetCellValue('G1', 'Author');
+		$sheet->SetCellValue('H1', 'Assignee');
 		$sheet->SetCellValue('I1', 'Created');
-		$sheet->SetCellValue('J1', 'Updated');
 			
 		// Define size of the columns
-		$sheet->getColumnDimension('A')->setWidth(6);
+		$sheet->getColumnDimension('A')->setWidth(75);
 		$sheet->getColumnDimension('B')->setWidth(16);
 		$sheet->getColumnDimension('C')->setWidth(12);
-		$sheet->getColumnDimension('D')->setWidth(72);
-		$sheet->getColumnDimension('E')->setWidth(22);
-		$sheet->getColumnDimension('F')->setWidth(22);
-		$sheet->getColumnDimension('G')->setWidth(65);
-		$sheet->getColumnDimension('H')->setWidth(10);
-		$sheet->getColumnDimension('I')->setWidth(26);
-		$sheet->getColumnDimension('J')->setWidth(26);
+		$sheet->getColumnDimension('D')->setWidth(65);
+		$sheet->getColumnDimension('E')->setWidth(50);
+		$sheet->getColumnDimension('F')->setWidth(10);
+		$sheet->getColumnDimension('G')->setWidth(22);
+		$sheet->getColumnDimension('H')->setWidth(22);
+		$sheet->getColumnDimension('I')->setWidth(18);
 
 		// Style
 		$styleArray = array( 'font' => array( 'bold' => true));
-		$objPHPExcel->getActiveSheet()->getStyle('A1:J1')->applyFromArray($styleArray);
+		$objPHPExcel->getActiveSheet()->getStyle('A1:I1')->applyFromArray($styleArray);
 
 
 		// Add data
 		$i = 2;
 		foreach ($arr_issues as $issue) {
-			$sheet->SetCellValue('A'.$i, $issue['id']);
+			$sheet->SetCellValue('A'.$i, '#'.$issue['id'].' - '.$issue['title']);
 			$sheet->SetCellValue('B'.$i, $issue['type']);
 			$sheet->SetCellValue('C'.$i, $issue['priority']);
-			$sheet->SetCellValue('D'.$i, $issue['title']);
-			$sheet->SetCellValue('E'.$i, $issue['reported_by']);
-			$sheet->SetCellValue('F'.$i, $issue['responsible']);
-			$sheet->SetCellValue('G'.$i, $issue['content']);
-			$sheet->getStyle('G'.$i)->getAlignment()->setWrapText(true);
-			$sheet->SetCellValue('H'.$i, $issue['status']);
-			$sheet->SetCellValue('I'.$i, $issue['created']);
-			$sheet->SetCellValue('J'.$i, $issue['last_updated']);
+			$sheet->SetCellValue('D'.$i, $issue['content']);
+			$sheet->getStyle('D'.$i)->getAlignment()->setWrapText(true);
+			$sheet->SetCellValue('E'.$i, $this->getLastComment($issue['id']));
+			$sheet->getStyle('E'.$i)->getAlignment()->setWrapText(true);
+			$sheet->SetCellValue('F'.$i, $issue['status']);
+			$sheet->SetCellValue('G'.$i, $issue['reported_by']);
+			$sheet->SetCellValue('H'.$i, $issue['responsible']);
+			$sheet->SetCellValue('I'.$i, $this->convertBitbucketDate($issue['created']));
 				
 			$i++;
 		}
